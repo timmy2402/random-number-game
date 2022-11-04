65c5550de58695a537f9252eb7f062d066cd9869
timmy2402 <saif.timms-samour@ada.ac.uk>
2022-11-04 12:15:11 +0000

    Working functions w/ Error handling

--- main.py
+++ main.py
@@ -9,5 +9,14 @@ def generateRandomNumber():
     return random.randint((-1 * sys.maxsize), sys.maxsize)
 
 
-def differenceFromAnswer(guess, answer):
-    pass
+def differenceFromAnswer(guess, answer = generateRandomNumber()):
+    print(answer)
+    try:  
+        if int(guess) < answer:
+            return 'Too low'
+        elif int(guess) > answer:
+            return 'Too high'
+        else:
+            return 'Correct'
+    except:
+      return 'Invalid'
